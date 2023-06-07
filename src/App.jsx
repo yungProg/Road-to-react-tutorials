f512d525044b9c3acdc012cc137be9f438730345
o-brown007 <gboakye97@yahoo.com>
2023-06-07 21:20:06 +0000

    replaced title string with an object and rendered it in <h1>.

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
 import * as React from "react";
 
-const title = "React"
+const welcome = {
+  title: "React",
+  greeting: "Hey"
+}
 
 function App(){
   return(
     <div>
-      <h1>Hello {title}</h1>
+      <h1>{welcome.greeting} {welcome['title']}</h1>
       <label htmlFor="search">Search: </label>
       <input id="search" type="text" />
     </div>
